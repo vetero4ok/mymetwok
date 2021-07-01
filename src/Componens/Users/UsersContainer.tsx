53a1014a7233be5cc483a5da0c1480cb675d053a
vetero4ok <82808215+vetero4ok@users.noreply.github.com>
2021-07-01 19:10:37 +0300

    we take out class Components in a userContainer and delete  UsersOnClassApiComponents.tsx (lessons 56)

--- src/Componens/Users/UsersContainer.tsx
+++ src/Componens/Users/UsersContainer.tsx
@@ -8,9 +8,55 @@ import {
     unfollowAC,
     userType
 } from '../../Redux/usersPageReducer';
-import UsersOnClassApiComponents from './UsersOnClassApiComponents';
 
+import React from 'react';
+import axios from 'axios';
+import {Users} from './Users';
 
+type PropsUsersType = {
+    users: Array<userType>
+    pageSize: number
+    totalUserCount: number
+    currentPages: number
+    follow: (userID: number) => void
+    unfollow: (userID: number) => void
+    setUsers: (users: Array<userType>) => void
+    setCurrantPage: (currentPages: number) => void
+    setTotalUserCount: (totalUsersCount: number) => void
+}
+class UsersApiComponents extends React.Component<PropsUsersType> {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPages}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUserCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrantPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return (
+            <Users
+                users={this.props.users}
+                pageSize={this.props.pageSize}
+                totalUserCount={this.props.totalUserCount}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+                currentPages={this.props.currentPages}
+                onPageChanged={this.onPageChanged}
+
+            />
+        );
+    }
+}
 
 let mapStateToProps = (state: AppStateType) => {
     return {
@@ -41,4 +87,4 @@ let mapDispatchToProps = (dispatch: AppDispatch) => {
 
 }
 
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersOnClassApiComponents)
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersApiComponents)
