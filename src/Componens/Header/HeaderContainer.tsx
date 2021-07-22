fa10d00e567fa55fa66da1211e7c9094a074ff51
vetero4ok <vetero4ok@gmail.com>
2021-07-22 21:24:50 +0300

    thunk for header(setUserAuthData) and refactor components

--- src/Componens/Header/HeaderContainer.tsx
+++ src/Componens/Header/HeaderContainer.tsx
@@ -3,38 +3,23 @@ import {Header} from './Header';
 import {connect} from 'react-redux';
 import {AppStateType} from '../../Redux/Redux-Store';
 import {setUserAuthData} from '../../Redux/authReducer';
-import {toggleIsFetching} from '../../Redux/usersPageReducer';
-import {setUserProfile, UserProfileType} from '../../Redux/profilePageReducer';
-import {authMeAPI, profileAPI,} from '../../Api/Api';
+import {UserProfileType} from '../../Redux/profilePageReducer';
+
 
 type PropsHeaderComponentType = {
     isAuth: boolean
     userId: number
     login: string
     email: string
-    setUserAuthData: (userId: number, email: string, login: string, isAuth: boolean) => void
-    toggleIsFetching: (isFetching: boolean) => void
-    setUserProfile: (data: UserProfileType) => void
     profile: UserProfileType | null
+    setUserAuthData: (userId: number) => void
 }
 
 class HeaderApiContainer extends React.Component<PropsHeaderComponentType> {
 
     componentDidMount() {
-        this.props.toggleIsFetching(true)
-        authMeAPI.authMe().then(response => {
-            if (response.data.resultCode === 0) {
-                let {id, email, login} = response.data.data
-                this.props.setUserAuthData(id, email, login, true)
-                profileAPI.getProfiles(this.props.userId)
-                    .then(response => {
-                        this.props.setUserProfile(response.data)
-                    })
-                this.props.toggleIsFetching(false)
-            }
+        this.props.setUserAuthData(this.props.userId)
 
-
-        })
     }
 
     render() {
@@ -60,7 +45,5 @@ function mapStateToProps(state: AppStateType) {
 
 export const HeaderContainer = connect(mapStateToProps, {
     setUserAuthData,
-    toggleIsFetching,
-    setUserProfile,
 
 })(HeaderApiContainer)
--- src/Redux/authReducer.tsx
+++ src/Redux/authReducer.tsx
@@ -1,3 +1,8 @@
+import {AppDispatch} from './Redux-Store';
+import {authMeAPI, profileAPI} from '../Api/Api';
+import {toggleIsFetching} from './usersPageReducer';
+import {setUserProfile} from './profilePageReducer';
+
 let InitialState: InitAuthStateType = {
     /** заглушка надо придумать как загружать данные
      * userId: 17817, login: '',  email: '',*/
@@ -19,10 +24,8 @@ const SET_USER_DATA = 'SET_USER_DATA' as const
 
 export type ActionTypeAuthReducer = SetUserAuthDataActionType
 
-export const authUserReducer = (
-    state = InitialState,
-    action: ActionTypeAuthReducer
-): InitAuthStateType => {
+export const authUserReducer =
+    (state = InitialState,action: ActionTypeAuthReducer): InitAuthStateType => {
     switch (action.type) {
         case SET_USER_DATA:
             return {
@@ -38,7 +41,7 @@ export const authUserReducer = (
 }
 
 
-export const setUserAuthData = (userId: number, email: string, login: string, isAuth: boolean ) => {
+export const setUserAuthDataSuccess = (userId: number, email: string, login: string, isAuth: boolean) => {
     return {
         type: SET_USER_DATA,
         userId,
@@ -48,4 +51,21 @@ export const setUserAuthData = (userId: number, email: string, login: string, is
     } as const
 }
 
-export type SetUserAuthDataActionType = ReturnType<typeof setUserAuthData>
+export type SetUserAuthDataActionType = ReturnType<typeof setUserAuthDataSuccess>
+
+export const setUserAuthData = (userId:number) => {
+    return (dispatch:AppDispatch)=>{
+        dispatch(toggleIsFetching(true));
+        authMeAPI.authMe().then(response => {
+            if (response.data.resultCode === 0) {
+                let {id, email, login} = response.data.data
+                dispatch(setUserAuthDataSuccess(id, email, login, true));
+                profileAPI.getProfiles(userId)
+                    .then(response => {
+                        dispatch(setUserProfile(response.data));
+                    })
+               dispatch(toggleIsFetching(false));
+            }
+        })
+    }
+}
